01026b9fd8ab3384681500824ce8e6a58e6e8a5c
Serban Bondor <serbanbondor@yahoo.com>
2020-04-25 13:38:23 +0300

    Updates

---
+++ precache-manifest.f1ffaec77b5643b2b4beb5ebae71347c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19e21bf70d519c0c2a18ee8af2c1892",
+    "url": "/pay-check/index.html"
+  },
+  {
+    "revision": "06355f83827c30b99f26",
+    "url": "/pay-check/static/css/main.065eb3e4.chunk.css"
+  },
+  {
+    "revision": "1301e593dc88def44ccf",
+    "url": "/pay-check/static/js/2.a1f82222.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pay-check/static/js/2.a1f82222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06355f83827c30b99f26",
+    "url": "/pay-check/static/js/main.0c7f50f6.chunk.js"
+  },
+  {
+    "revision": "3d8cde72ace3e6146e03",
+    "url": "/pay-check/static/js/runtime-main.4b8253e8.js"
+  }
+]);
