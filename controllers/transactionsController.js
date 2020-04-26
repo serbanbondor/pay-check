15b6b26f83a5775059661ddac6723b1856bf0736
Serban Bondor <serbanbondor@yahoo.com>
2020-04-26 18:55:19 +0300

    added get, post and delete methods to api

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,6 +1,20 @@
 // @desc Get all transactions
 // @route GET /api/transactions
-// method that sends a string and is exported
+// method that gets info and is imported in routes
 exports.getTransactions = (req, res, next) => {
   res.send('GET transactions');
+};
+
+// @desc Add transaction
+// @route POST /api/transactions
+// method that sends  info and is imported in routes
+exports.addTransactions = (req, res, next) => {
+  res.send('POST transactions');
+};
+
+// @desc Delete transaction
+// @route DELETE /api/transactions:id
+// method that deletes info and is imported in routes
+exports.deleteTransactions = (req, res, next) => {
+  res.send('DELETE transactions');
 };
--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions } = require('../controllers/transactionsController');
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransactions,
+} = require('../controllers/transactionsController');
 
-// import the method from the controller and this gets send to the api
-router.route('/').get(getTransactions);
+// import the method from the controller and this gets send to the api - used for GET and POST
+router.route('/').get(getTransactions).post(addTransactions);
+
+// import the method from the controller and this gets send to the api - used for DELETE(needs id)
+router.route('/:id').delete(deleteTransactions);
 
 module.exports = router;
