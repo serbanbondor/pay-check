ef5ba80d3229bb4149e26b36d6953331552ec4d3
Serban Bondor <serbanbondor@yahoo.com>
2020-04-26 20:39:29 +0300

    changed model file name

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,20 +1,22 @@
+const TransactionModel = require('../models/TransactionModel');
+
 // @desc Get all transactions
 // @route GET /api/transactions
 // method that gets info and is imported in routes
-exports.getTransactions = (req, res, next) => {
+exports.getTransactions = async (req, res, next) => {
   res.send('GET transactions');
 };
 
 // @desc Add transaction
 // @route POST /api/transactions
 // method that sends  info and is imported in routes
-exports.addTransactions = (req, res, next) => {
+exports.addTransactions = async (req, res, next) => {
   res.send('POST transactions');
 };
 
 // @desc Delete transaction
 // @route DELETE /api/transactions:id
 // method that deletes info and is imported in routes
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions = async (req, res, next) => {
   res.send('DELETE transactions');
 };
